0a175e86ad295ba16d1d64a00bebc81f93f689d6
kp0hyc <kp0hyc@mail.ru>
2017-11-18 14:39:58 +0300

    create camera preview
    open camera preview and catched all exceptions

--- app/src/main/java/com/a132room/kp0hyc/videoeditor/TabActivity.java
+++ app/src/main/java/com/a132room/kp0hyc/videoeditor/TabActivity.java
@@ -7,10 +7,12 @@ import android.content.Context;
 import android.content.pm.PackageManager;
 import android.hardware.camera2.CameraAccessException;
 import android.hardware.camera2.CameraCaptureSession;
+import android.hardware.camera2.CameraCharacteristics;
 import android.hardware.camera2.CameraDevice;
 import android.hardware.camera2.CameraManager;
 import android.hardware.camera2.CaptureRequest;
 import android.hardware.camera2.params.OutputConfiguration;
+import android.hardware.camera2.params.StreamConfigurationMap;
 import android.os.Build;
 import android.os.Handler;
 import android.support.annotation.NonNull;
@@ -39,6 +41,7 @@ import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.IllegalFormatException;
 import java.util.Iterator;
 import java.util.List;
 import java.util.ListIterator;
@@ -54,9 +57,7 @@ public class TabActivity extends AppCompatActivity {
      * {@link android.support.v4.app.FragmentStatePagerAdapter}.
      */
     private SectionsPagerAdapter mSectionsPagerAdapter;
-    private static final int MY_PERMISSIONS_REQUEST_CAMERA = 0;
     private ViewPager mViewPager;
-    private CameraManager cameraManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -72,116 +73,24 @@ public class TabActivity extends AppCompatActivity {
         // Set up the ViewPager with the sections adapter.
         mViewPager = (ViewPager) findViewById(R.id.container);
         mViewPager.setAdapter(mSectionsPagerAdapter);
-        mViewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
-            @Override
-            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-
-            }
-
-            @Override
-            public void onPageSelected(int position) {
-                Log.d("kp0hyc_debug", "onPageSelected");
-                if (position == 0)
-                    reOpenCamera(null);
-            }
-
-            @Override
-            public void onPageScrollStateChanged(int state) {
-
-            }
-        });
     }
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode,
-                                           String permissions[], int[] grantResults) {
-        switch (requestCode) {
-            case MY_PERMISSIONS_REQUEST_CAMERA: {
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    tryOpenCamera();
-                }
-                return;
-            }
-        }
-    }
-
-    public void reOpenCamera(View v)
-    {
-        if (Build.VERSION.SDK_INT >= 21)
-        {
-            Log.d("kp0hyc_debug", "enough version");
-            cameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
-
-            if (Build.VERSION.SDK_INT >= 23)
-                if (checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED)
-                    tryOpenCamera();
-                else
-                {
-                    requestPermissions(new String[]{Manifest.permission.CAMERA}, MY_PERMISSIONS_REQUEST_CAMERA);
-                }
-            else {
-                tryOpenCamera();
-            }
-        } else {
-            tryOpenCamera();
-        }
-    }
-
-    private void tryOpenCamera() {
-        if (Build.VERSION.SDK_INT >= 21) {
-            try {
-                cameraManager.openCamera(cameraManager.getCameraIdList()[0], new CameraCallback(), null);
-            } catch (SecurityException e) {
-                Log.d("kp0hyc_debug", "Security Exception");
-            } catch (CameraAccessException e) {
-                Log.d("kp0hyc_debug", "Camera Access Exception");
-            }
-        } else
-            Log.d("kp0hyc_debug", "Low version of android");
-    }
-
-
-    /*@Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_tab, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }*/
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
     public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
 
         private static final String ARG_SECTION_NUMBER = "section_number";
+        private static final int MY_PERMISSIONS_REQUEST_CAMERA = 0;
+
+        private String currentCam;
+        private CameraManager cameraManager;
+        private Surface surface = null;
+        private CameraDevice cameraDevice = null;
+        private View rootView;
+        private boolean windowOpened = false;
+        private boolean cameraStarted = false;
 
         public PlaceholderFragment() {
         }
 
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
         public static PlaceholderFragment newInstance(int sectionNumber) {
             PlaceholderFragment fragment = new PlaceholderFragment();
             Bundle args = new Bundle();
@@ -193,19 +102,220 @@ public class TabActivity extends AppCompatActivity {
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
-            if (getArguments().getInt(ARG_SECTION_NUMBER) == 3)
-                return inflater.inflate(R.layout.third_window, container, false);
-            View rootView = inflater.inflate(R.layout.fragment_tab, container, false);
-            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-            textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
-            return rootView;
+            switch (getArguments().getInt(ARG_SECTION_NUMBER))
+            {
+                case 2:
+                    return inflater.inflate(R.layout.third_window, container, false);
+                case 0:
+                    rootView = inflater.inflate(R.layout.fragment_tab, container, false);
+                    TextView textView = (TextView) rootView.findViewById(R.id.section_label);
+                    textView.setText(getString(R.string.section_format, 1));
+                    View surface = rootView.findViewById(R.id.surfaceView);
+                    surface.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            reOpenCamera();
+                        }
+                    });
+                    rootView.addOnAttachStateChangeListener(new View.OnAttachStateChangeListener() {
+                        @Override
+                        public void onViewAttachedToWindow(View view) {
+                            Log.d("kp0hyc_debug", "view attached");
+                            windowOpened = true;
+                            /*if (cameraDevice == null)
+                                reOpenCamera();*/
+                        }
+
+                        @Override
+                        public void onViewDetachedFromWindow(View view) {
+                            Log.d("kp0hyc_debug", "view detached");
+                            windowOpened = false;
+                        }
+                    });
+                    return rootView;
+                case 1:
+                    return inflater.inflate(R.layout.second_window, container, false);
+                default:
+                    return null;
+            }
+        }
+
+        @Override
+        public void onResume()
+        {
+            super.onResume();
+            Log.d("kp0hyc_debug", "on resume");
+            if (windowOpened && cameraDevice == null) {
+                Log.d("kp0hyc_debug", "on resume open camera");
+                reOpenCamera();
+            }
+        }
+
+        public void reOpenCamera()
+        {
+            if (Build.VERSION.SDK_INT >= 21)
+            {
+                Log.d("kp0hyc_debug", "enough version");
+                cameraManager = (CameraManager) getContext().getSystemService(Context.CAMERA_SERVICE);
+
+                if (Build.VERSION.SDK_INT >= 23)
+                    if (getContext().checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED)
+                        tryOpenCamera();
+                    else
+                    {
+                        requestPermissions(new String[]{Manifest.permission.CAMERA}, MY_PERMISSIONS_REQUEST_CAMERA);
+                    }
+                else {
+                    tryOpenCamera();
+                }
+            } else {
+                Log.d("kp0hyc_debug", "this device in progress");
+            }
+        }
+
+        @TargetApi(21)
+        private void startCameraSession()
+        {
+            try {
+                if (surface == null) {
+                    Log.d("kp0hyc_debug", "surface not ready");
+                    return;
+                }
+                if (cameraDevice == null) {
+                    Log.d("kp0hyc_debug", "camera not ready");
+                    return;
+                }
+
+                List<Surface> l = new ArrayList<>();
+
+                l.add(surface);
+
+                Log.d("kp0hyc_debug", "creating capture session");
+
+                cameraDevice.createCaptureSession(l, new MyStateCallback(), null);
+            } catch (CameraAccessException e) {
+            }
+        }
+
+        private void tryOpenCamera() {
+            if (Build.VERSION.SDK_INT >= 21) {
+                try {
+                    //setting up surface
+                    if (rootView.findViewById(R.id.surfaceView) == null)
+                    {
+                        Log.d("kp0hyc_debug", "surface not opened");
+                        return;
+                    }
+                    SurfaceHolder sh = ((SurfaceView) rootView.findViewById(R.id.surfaceView)).getHolder();
+                    currentCam = cameraManager.getCameraIdList()[0];
+                    StreamConfigurationMap config = cameraManager.getCameraCharacteristics(currentCam).
+                            get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP);
+                    sh.addCallback(new MySurfaceHolderCallback());
+                    sh.setFixedSize(config.getHighSpeedVideoSizes()[0].getHeight(), config.getHighSpeedVideoSizes()[0].getWidth());
+                    Log.d("kp0hyc_debug", "started setting up surface");
+
+                    cameraManager.openCamera(currentCam, new CameraCallback(), null);
+                    Log.d("kp0hyc_debug", "started setting up camera");
+                } catch (SecurityException e) {
+                    Log.d("kp0hyc_debug", "Security Exception");
+                } catch (CameraAccessException e) {
+                    Log.d("kp0hyc_debug", "Camera Access Exception");
+                }
+            } else
+                Log.d("kp0hyc_debug", "Low version of android");
+        }
+
+        @Override
+        public void onRequestPermissionsResult(int requestCode,
+                                               String permissions[], int[] grantResults) {
+            switch (requestCode) {
+                case MY_PERMISSIONS_REQUEST_CAMERA: {
+                    if (grantResults.length > 0
+                            && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                        tryOpenCamera();
+                    }
+                    return;
+                }
+            }
+        }
+
+        @TargetApi(21)
+        private class CameraCallback extends CameraDevice.StateCallback {
+
+            @Override
+            public void onOpened(@NonNull CameraDevice cd) {
+                Log.d("kp0hyc_debug", "camera onOpened");
+                cameraDevice = cd;
+                startCameraSession();
+            }
+
+            @Override
+            public void onDisconnected(@NonNull CameraDevice cd) {
+                Log.d("kp0hyc_debug", "camera onDisconnected");
+                cameraDevice.close();
+                cameraDevice = null;
+            }
+
+            @Override
+            public void onError(@NonNull CameraDevice cd, int i) {
+                Log.d("kp0hyc_debug", " camera onError");
+                cameraDevice.close();
+                cameraDevice = null;
+            }
+        }
+
+        @TargetApi(21)
+        class MyStateCallback extends CameraCaptureSession.StateCallback
+        {
+
+            @Override
+            public void onConfigured(@NonNull CameraCaptureSession cameraCaptureSession) {
+                Log.d("kp0hyc_debug", " camera onConfigured " + Integer.toString(cameraCaptureSession.hashCode()));
+                try {
+                    cameraCaptureSession.abortCaptures();
+                    CaptureRequest.Builder previewRequestBuilder = cameraDevice
+                            .createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
+                    previewRequestBuilder.addTarget(surface);
+                    cameraCaptureSession.setRepeatingRequest(previewRequestBuilder.build(),
+                            null, null);
+                } catch (CameraAccessException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onConfigureFailed(@NonNull CameraCaptureSession cameraCaptureSession) {
+                Log.d("kp0hyc_debug", " camera onConfigureFailed");
+            }
+        }
+
+        class MySurfaceHolderCallback implements SurfaceHolder.Callback
+        {
+
+            @Override
+            public void surfaceCreated(SurfaceHolder surfaceHolder) {
+                Log.d("kp0hyc_debug", "surface configured");
+                //surface = surfaceHolder.getSurface();
+                startCameraSession();
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+                Log.d("kp0hyc_debug", "surface changed");
+                surface = surfaceHolder.getSurface();
+                startCameraSession();
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+                Log.d("kp0hyc_debug", "surface destroyed");
+                cameraDevice.close();
+                surface = null;
+                cameraDevice = null;
+            }
         }
     }
 
-    /**
-     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
-     * one of the sections/tabs/pages.
-     */
     public class SectionsPagerAdapter extends FragmentPagerAdapter {
 
         public SectionsPagerAdapter(FragmentManager fm) {
@@ -214,61 +324,13 @@ public class TabActivity extends AppCompatActivity {
 
         @Override
         public Fragment getItem(int position) {
-            // getItem is called to instantiate the fragment for the given page.
-            // Return a PlaceholderFragment (defined as a static inner class below).
-            return PlaceholderFragment.newInstance(position + 1);
+            return PlaceholderFragment.newInstance(position);
         }
 
         @Override
         public int getCount() {
             // Show 3 total pages.
             return 3;
-        }
-    }
-
-    @TargetApi(21)
-    private class CameraCallback extends CameraDevice.StateCallback {
-
-        @Override
-        public void onOpened(@NonNull CameraDevice cameraDevice) {
-            SurfaceView sv = (SurfaceView) findViewById(R.id.surfaceView);
-            if (sv == null)
-                Log.d("kp0hyc_debug", "it is null");
-            /*SurfaceHolder sh = ((SurfaceView) findViewById(R.id.surfaceView)).getHolder();
-            //ToDo: make fixed sizes
-            //sh.setFixedSize(100, 100);
-            List<Surface> l = new ArrayList<>();
-            l.add(sh.getSurface());
-            try {
-                cameraDevice.createCaptureSession(l, new MyStateCallback(), null);
-            } catch (CameraAccessException e) {
-                Log.d("kp0hyc_debug", "CameraAccessException in onoOpened");
-            }
-            Log.d("kp0hyc_debug", "camera onOpened");*/
-        }
-
-        @Override
-        public void onDisconnected(@NonNull CameraDevice cameraDevice) {
-            Log.d("kp0hyc_debug", "camera onDisconnected");
-        }
-
-        @Override
-        public void onError(@NonNull CameraDevice cameraDevice, int i) {
-            Log.d("kp0hyc_debug", " camera onError");
-        }
-
-        class MyStateCallback extends CameraCaptureSession.StateCallback
-        {
-
-            @Override
-            public void onConfigured(@NonNull CameraCaptureSession cameraCaptureSession) {
-                Log.d("kp0hyc_debug", " camera onConfigured");
-            }
-
-            @Override
-            public void onConfigureFailed(@NonNull CameraCaptureSession cameraCaptureSession) {
-                Log.d("kp0hyc_debug", " camera onConfigureFailed");
-            }
         }
     }
 }
